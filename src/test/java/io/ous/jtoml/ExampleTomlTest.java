628bb9c17c158d23ad481b1d45e48e37484b781b
Asafh <asafh.gh@ous.im>
2013-03-01 19:19:33 +0200

    Added Unit Tests based on agrison/jtoml's Unit Tests

---
+++ src/test/java/io/ous/jtoml/ExampleTomlTest.java
@@ -0,0 +1,131 @@
+package io.ous.jtoml;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * This file is under MIT License, based on https://github.com/agrison/jtoml/blob/master/src/test/java/me/grison/jtoml/ExampleTomlTest.java <br/>
+ * MIT License: http://opensource.org/licenses/mit-license.php
+ * @author Asafh
+ *
+ */
+public class ExampleTomlTest {
+	private static Toml toml;
+
+	@BeforeClass
+	public static void parseExampleFile() throws IOException {
+        toml = JToml.parse(ExampleTomlTest.class.getResourceAsStream("/example.toml"));
+	}
+
+	@Test
+	public void testTopLevelString() {
+		// title = "TOML Example"
+		assertEquals("TOML Example", toml.getString("title"));
+	}
+
+	@Test
+	public void testNestedStrings() {
+		// [owner]
+		// name = "Tom Preston-Werner"
+		// organization = "GitHub"
+		assertEquals("Tom Preston-Werner", toml.getString("owner.name"));
+		assertEquals("GitHub", toml.getString("owner.organization"));
+	}
+
+	@Test
+	public void testNewlineAndHashInString() {
+		// [owner]
+		// bio = "GitHub Cofounder & CEO\nLikes tater tots and beer #awesome."
+		assertEquals(
+				"GitHub Cofounder & CEO\nLikes tater tots and beer #awesome.",
+				toml.getString("owner.bio"));
+	}
+
+	@Test
+	public void testDate() throws ParseException, ClassCastException, java.text.ParseException {
+		// dob = 1979-05-27T07:32:00Z # First class dates? Why not?
+		assertEquals(
+				createCalendar("yyyy-MM-dd-HH:mm:ssZ",
+						"1979-05-27-07:32:00-0000").getTime(), toml.getDate("owner.dob"));
+	}
+
+	@Test
+	public void testArray() {
+		// [database]
+		// server = "192.168.1.1"
+		// ports = [ 8001, 8001, 8002 ]
+		assertEquals("192.168.1.1", toml.getValue("database.server"));
+		assertEquals(
+				createList(Long.valueOf(8001), Long.valueOf(8001), Long.valueOf(8002)),
+                toml.getList("database.ports"));
+	}
+
+	@Test
+	public void testLong() {
+		// [database]
+		// connection_max = 5000
+		// latency_max = 42 # this is in milliseconds
+		assertEquals(Long.valueOf(5000),
+				toml.getLong("database.connection_max"));
+		assertEquals(Long.valueOf(42), toml.getLong("database.latency_max"));
+	}
+
+	@Test
+	public void testBoolean() {
+		// [database]
+		// enabled = true
+		// awesome = false # just because
+		assertEquals(Boolean.valueOf(true), toml.getBoolean("database.enabled"));
+		assertEquals(Boolean.valueOf(false),
+				toml.getBoolean("database.awesome"));
+	}
+
+	@Test
+	public void testIndentationDoesntMatter() {
+		// [servers]
+		//
+		// # You can indent as you please. Tabs or spaces. TOML don't care.
+		// [servers.alpha]
+		// ip = "10.0.0.1"
+		// dc = "eqdc10"
+		assertEquals("10.0.0.1", toml.getValue("servers.alpha.ip"));
+		assertEquals("eqdc10", toml.getValue("servers.alpha.dc"));
+
+		// [servers.beta]
+		// ip = "10.0.0.2"
+		// dc = "eqdc10"
+		assertEquals("10.0.0.2", toml.getValue("servers.beta.ip"));
+		assertEquals("eqdc10", toml.getValue("servers.beta.dc"));
+	}
+
+	@Test
+	public void testArrayOfArrays() {
+		// [clients]
+		// data = [ ["gamma", "delta"], [1, 2] ] # just an update to make sure
+		// parsers support it
+		assertEquals(ExampleTomlTest.<Object> createList(
+				createList("gamma", "delta"),
+				createList(Long.valueOf(1), Long.valueOf(2))),
+				toml.getList("clients.data"));
+	}
+
+	private static Calendar createCalendar(String pattern, String value)
+			throws ParseException, java.text.ParseException {
+		Date date = new SimpleDateFormat(pattern).parse(value);
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		return calendar;
+	}
+
+	private static <T> List<T> createList(T... elements) {
+		return Arrays.<T> asList(elements);
+	}
+}
---
+++ src/test/java/io/ous/jtoml/SpecificationTest.java
@@ -0,0 +1,147 @@
+package io.ous.jtoml;
+
+import io.ous.jtoml.impl.KeyGroup;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * This file is under the MIT License, based on https://github.com/agrison/jtoml/blob/master/src/test/java/me/grison/jtoml/SpecificationsTest.java <br/>
+ * MIT License: http://opensource.org/licenses/mit-license.php
+ * @author Asafh
+ *
+ */
+
+public class SpecificationTest {
+	@Test
+	public void testInteger() throws IOException {
+		Toml toml = JToml.parseString("foo = 42");
+		Assert.assertEquals(42L, toml.getLong("foo").longValue());
+	}
+
+	@Test
+	public void testDouble() throws IOException {
+		Toml toml = JToml.parseString("foo = 3.141592653589793");
+		Assert.assertEquals(3.14159265, toml.getDouble("foo").doubleValue(), 0.00001d);
+	}
+
+	@Test
+	public void testBoolean() throws IOException {
+		Toml toml = JToml.parseString("foo = true\nbar = false");
+		Assert.assertTrue(toml.getBoolean("foo").booleanValue());
+		Assert.assertFalse(toml.getBoolean("bar").booleanValue());
+	}
+
+	@Test
+	public void testString() throws IOException {
+		Toml toml = JToml.parseString("foo = \"Hello\\tWorld\\nI'm having \\\"!\"");
+		Assert.assertEquals("Hello\tWorld\nI'm having \"!", toml.getString("foo"));
+	}
+
+	@Test
+	public void testArray() throws IOException {
+		Toml toml = JToml.parseString("foo = [\n\"Hello\",\n\n\t \"World\"\n,\"Nice\"]");
+		Assert.assertEquals(Arrays.asList("Hello", "World", "Nice"), toml.getList("foo"));
+	}
+
+	@Test(expected = ParseException.class)
+	public void testWrongStringEscaping() throws IOException {
+		// wrong = "C:\Users\nodejs\templates" # note: doesn't produce a valid path
+		// right = "C:\\Users\\nodejs\\templates"
+		Toml toml = JToml.parseString("right = \"C:\\\\Users\\\\nodejs\\\\templates\"");
+		Assert.assertEquals("C:\\Users\\nodejs\\templates", toml.getValue("right"));
+		toml = JToml.parseString("wrong = \"C:\\Users\\nodejs\\templates\"");
+	}
+	
+	@Test(expected = ParseException.class) //Parse exception
+	public void testOverwritePreviousKey() throws IOException {
+		JToml.parseString("[fruit]\ntype = \"apple\"\n\n[fruit.type]\napple = \"yes\"");
+	}
+
+	@Test
+	public void testNull() throws IOException {
+		Toml toml = JToml.parseString("foo = 1337");
+		Assert.assertEquals(null, toml.getValue("bar"));
+		Assert.assertEquals(null, toml.getString("bar"));
+		Assert.assertEquals(null, toml.getLong("bar"));
+		Assert.assertEquals(null, toml.getDouble("bar"));
+		Assert.assertEquals(null, toml.getDate("bar"));
+		Assert.assertEquals(null, toml.getBoolean("bar"));
+		Assert.assertEquals(null, toml.getList("bar"));
+	}
+
+	@Test(expected = ClassCastException.class)
+	public void testIncompatibleType() throws IOException {
+		Toml toml = JToml.parseString("foo = 1337");
+		toml.getString("foo");
+	}
+
+	@Test
+	public void testLocalsAsMap() throws IOException {
+		Toml toml = JToml.parseString("[foo]\nbar = true\nbaz = false");
+		Map<String, Object> map = toml.getKeyGroup("foo").localsAsMap();
+		Assert.assertTrue(map.containsKey("bar") && map.get("bar").equals(Boolean.TRUE));
+		Assert.assertTrue(map.containsKey("baz") && map.get("baz").equals(Boolean.FALSE));
+	}
+	@Test
+	public void testAsMap() throws IOException {
+		Toml toml = JToml.parseString("x=\"A\"\n[foo]\nbar = true\nbaz = false");
+		
+		Map<String, Object> map = toml.toMap();
+		Assert.assertTrue(map.containsKey("foo") && ((KeyGroup) map.get("foo")).getName().equals("foo"));
+		
+		Assert.assertTrue(map.containsKey("foo.bar") && map.get("foo.bar").equals(Boolean.TRUE));
+		Assert.assertTrue(map.containsKey("foo.baz") && map.get("foo.baz").equals(Boolean.FALSE));
+	}
+
+	@Test
+	public void testCustomObject() throws IOException {
+		Toml toml = JToml.parseString("[foo]\nstringKey=\"a\"\nlongKey=42\ndoubleKey=13.37\n" + //
+				"booleanKey=true\nlistKey=[1,2,3]\n[foo.bar]\nbazz=\"Hello\"\ndummy=459");
+		Foo foo = toml.getKeyGroup("foo").asObject(Foo.class);
+		Assert.assertEquals("a", foo.stringKey);
+		Assert.assertEquals(Long.valueOf(42), foo.longKey);
+		Assert.assertEquals(Double.valueOf(13.37), foo.doubleKey, 0.00001d);
+		Assert.assertEquals(Boolean.TRUE, foo.booleanKey);
+		Assert.assertEquals(Arrays.asList(1L, 2L, 3L), foo.listKey);
+		Assert.assertEquals("Hello", foo.bar.bazz);
+		Assert.assertEquals(Long.valueOf(459), foo.bar.dummy);
+		// test no root group
+		toml = JToml.parseString("stringKey=\"a\"\nlongKey=42\ndoubleKey=13.37\n" + //
+				"booleanKey=true\nlistKey=[1,2,3]\n[bar]\nbazz=\"Hello\"\ndummy=459");
+		Assert.assertEquals(foo.toString(), toml.asObject(Foo.class).toString());
+	}
+
+	/**
+	 * Simple class tested above.
+	 */
+	public static class Bar {
+		String bazz;
+		Long dummy;
+
+		@Override
+		public String toString() {
+			return bazz + dummy;
+		}
+	}
+
+	public static class Foo {
+		String stringKey;
+		Long longKey;
+		Double doubleKey;
+		Boolean booleanKey;
+		List<Object> listKey;
+		Bar bar;
+		Boolean awesome;
+
+		@Override
+		public String toString() {
+			return stringKey + longKey + doubleKey + booleanKey + listKey + bar + awesome;
+		}
+	}
+}
